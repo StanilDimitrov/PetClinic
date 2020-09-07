f2b74f03b771cb6440b83440a72f3860152cd66d
Stenli <stanil_dimitrov@mail.bg>
2020-09-07 17:22:15 +0300

    Add custom exceptions and constants.

---
+++ src/Domain/PetClinic.Domain/Constants/PhoneNumberConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace PetClinic.Domain.Constants
+{
+    public class PhoneNumberConstants
+    {
+        public const int MinPhoneNumberLength = 5;
+        public const int MaxPhoneNumberLength = 20;
+        public const string PhoneNumberRegularExpression = @"\+[0-9]*";
+    }
+}
