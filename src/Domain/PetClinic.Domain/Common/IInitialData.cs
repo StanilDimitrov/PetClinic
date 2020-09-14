d1165ef36866a013ef2e081010c121a394b72528
Stenli <stanil_dimitrov@mail.bg>
2020-09-14 08:50:09 +0300

    Add domain configuration for registering services in domain layer and IInitialData interface.

---
+++ src/Domain/PetClinic.Domain/Common/IInitialData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetClinic.Domain.Common
+{
+    public interface IInitialData
+    {
+        Type EntityType { get; }
+
+        IEnumerable<object> GetData();
+    }
+}
